af6b263eb4e07daf5ccdbf9dc57fe417e685bd74
Matt Geller <mattg92@gmail.com>
2022-10-14 16:10:02 -0700

    Got it to green

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/projects', function () {
+    $projects = Project::all();
+
+    return view('projects.index', compact('projects'));
+});
+
+Route::post('/projects', function () {
+    //validate
+
+    Project::create(request(['title', 'description']));
 });
